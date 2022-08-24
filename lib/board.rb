02107a857d7dc84d4681847b6c40d772165b7893
Aodj05 <aodj05@msn.com>
2022-08-23 20:17:11 -0400

    Setup Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,6 @@
 class Board
+    attr_reader :grid
+
     def initialize
         @grid = [
             ["X", "X", nil, "X"],
@@ -8,8 +10,23 @@ class Board
         ]
     end
 
-    def place(piece, row, column)
-        @grid[row][column] = piece
+    def [] = (location, piece)
+        row, column = location
+        grid[row][column] = piece
     end
-    
+
+    def [](location)
+        row, column = location
+        grid[row][column]
+    end
+
+    def in_bounds?(location)
+        row, column = location
+        
+        row < grid.length &&
+        column < grid.first.length &&
+        row >= 0 &&
+        column >= 0
+    end
 end
+
